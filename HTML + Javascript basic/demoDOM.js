068926080802c9f20a3497d352688af3f7d128e4
khuong <tranvinhkhuong@gmail.com>
2020-02-25 12:39:09 -0800

    new stuff

---
+++ HTML + Javascript basic/demoDOM.js
@@ -0,0 +1,12 @@
+// var p = document.querySelector("p");
+// p.textContent
+// return "Fun thing to do"
+
+// p.innerHTML
+// return "Fun thing <strong>to do</strong>"
+
+// document.querySelector("h1").textContent = "THIS IS THE END"
+// the h1 line "Welcome to DOMland" will change to "THIS IS THE END"
+
+// document.body.innerHTML = "<h1>This is GoodBye!!!</h1>"
+// entire content replace with "<h1>This is GoodBye!!!</h1>"
