927a13ca81a3d0cc80898fe4ad7190257bfeb960
khuong <tranvinhkhuong@gmail.com>
2020-02-24 22:42:32 -0800

    new stuff

--- Array Methods/array.js
+++ Array Methods/array.js
@@ -10,23 +10,28 @@ arr.push(10)
 //POP
 //removing element from the end of an Array
 //SYNTAX: array.pop()
-//no require parameter
+//NO require parameter
 arr.pop()
 //Expect arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 //UNSHIFT
 //adding element at the front of an Array
-arr.unshift(0)
+//SYNTAX: array.unshift(item1, item2, ...)
 //require parameter
+arr.unshift(0)
+
 //Expect arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 //SHIFT
 //removing element from the front of an Array
+//SYNTAX: array.shift()
+//NO require parameter
 arr.shift()
 //Expect arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 //SPLICE
 //Insertion and Removal in between an Array
+//SYNTAX: array.splice(start, deleteCount, item1, item2, ...)
 //array.splice(start (location of item you want to start), deleteCount (how many item you want to delete), item1, item2, ...)
 arr.splice(3, 2);
 //As we input 3 mean in array at position 3 we start, then we have 2 meaning we delete 2 item from that array start from position 3. In this case its mean number 4 and 5 will be delete.
