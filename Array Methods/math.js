4094a8e2055c8149c00f0bacd2268aae708253fd
khuong <tranvinhkhuong@gmail.com>
2020-03-04 22:33:17 -0800

    new stuff

--- Array Methods/math.js
+++ Array Methods/math.js
@@ -7,3 +7,14 @@ Math.random()
 Math.PI
 // always return pi number
 // Expect returns 3.141592653589793
+
+Math.round()
+// return the value of x rounded to its nearest integer
+// Example Math.round(4.7)
+// Expect 5
+
+Math.pow()
+// SYNTAX: Math.pow(x, y)
+// return the value of x to the power of y
+// Example Math.pow(8, 2)
+// Expect 64 as 8^2 = 64
