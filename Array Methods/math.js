f9f92e0f3135151f6b4dd26fb5bc1edb7177ebb8
khuong <tranvinhkhuong@gmail.com>
2020-03-04 22:29:56 -0800

    new stuff

---
+++ Array Methods/math.js
@@ -0,0 +1,9 @@
+Math.random()
+// return a random number between 0 and 1
+// if you want to get number more than 1 but less than 10. Trick is Math.random() * 10
+// depend on the range of the number you want to show like range from 0 to 500
+// use Math.random() * "last number of the range you want"
+
+Math.PI
+// always return pi number
+// Expect returns 3.141592653589793
