b07db2ef4e79d8bd2f2d6991115c5843a683897e
khuong <tranvinhkhuong@gmail.com>
2020-04-06 19:54:00 -0700

    new lessons

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -7,22 +7,31 @@ dotenv.config();
 const port = parseInt(process.env.APP_PORT || 3000)
 app.set('view engine', 'ejs')
 
+var campgrounds = [
+    { name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg" },
+    { name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142_960_720.jpg" },
+    { name: "Mount Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg" }
+]
+
 app.get("/", (req, res) => {
     res.render("landing")
 })
 
 app.get("/campgrounds", (req, res) => {
-    var campgrounds = [
-        { name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_960_720.jpg" },
-        { name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142_960_720.jpg" },
-        { name: "Mount Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg" }
-    ]
 
     res.render("campgrounds", { campgrounds: campgrounds })
 })
 
+app.get("/campgrounds/news", (req, res) => {
+    res.render("news.ejs")
+})
+
 app.post("/campgrounds", (req, res) => {
-    res.send("YOU HIT THE POST ROUTE")
+    const name = req.body.name;
+    const image = req.body.image;
+    const newCampgrounds = { name: name, image: image }
+    campgrounds.push(newCampgrounds)
+    res.redirect("/campgrounds")
 })
 
 app.listen(port, () => {
