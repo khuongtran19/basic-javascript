067fe8539ad7760ac1ec63891ca7589b2fb5ca27
khuong <tranvinhkhuong@gmail.com>
2020-04-21 19:52:44 -0700

    new lessons

--- Associations/references.js
+++ Associations/references.js
@@ -1,29 +1,37 @@
 const mongoose = require("mongoose");
+const Post = require("./models/post")
+const User = require("./models/user")
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/blog_demo_2", { useFindAndModify: false });
-
-// Post - title, content
-const postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-})
-const Post = mongoose.model("Post", postSchema);
-
-//User - email, name
-const userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-    }]
-})
-const User = mongoose.model("User", userSchema);
 
 Post.create({
     title: "How to cook the best burger",
     content: "blah blah blah"
 }, (err, post) => {
-    console.log(post)
+    User.findOne({ email: "kev@gmail.com" }, (err, foundUser) => {
+        if (err) {
+            console.log(err)
+        } else {
+            foundUser.posts.push(post)
+            foundUser.save((err, data) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(data)
+                }
+            })
+        }
+    })
 })
+
+// Find user
+// Find all posts for that user
+
+// User.findOne({ email: "kev@gmail.com" }).populate("posts").exec((err, user) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(user)
+//     }
+// })
