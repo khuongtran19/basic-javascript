98a3fad17301a5409f31200ab22f659ba817c157
khuong <tranvinhkhuong@gmail.com>
2020-04-23 14:50:37 -0700

    new lessons

--- YelpCampWeb/routes/campgrounds.js
+++ YelpCampWeb/routes/campgrounds.js
@@ -51,18 +51,14 @@ router.get("/:id", (req, res) => {
 })
 
 // Edit campground route
-router.get("/edit", (req, res) => {
+router.get("/edit", checkCampgroundOwnership, (req, res) => {
     Campground.findById(req.params.id, (err, foundCampground) => {
-        if (err) {
-            res.redirect("/campgrounds")
-        } else {
-            res.render("/campgrounds/edit", { campground: foundCampground })
-        }
+        res.render("/campgrounds/edit", { campground: foundCampground })
     })
 })
 
 // Update campground route
-router.put("/:id", (req, res) => {
+router.put("/:id", checkCampgroundOwnership, (req, res) => {
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
         if (err) {
@@ -73,12 +69,41 @@ router.put("/:id", (req, res) => {
     })
 })
 
+// Delete campground route
+router.delete("/:id", checkCampgroundOwnership, (req, res) => {
+    Campground.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            res.redirect("/campgrounds")
+        } else {
+            res.redirect("/campgrounds")
+        }
+    })
+})
+
 // middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login")
+}
+
+function checkCampgroundOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, (err, foundCampground) => {
+            if (err) {
+                res.redirect("back")
+            } else {
+                if (foundCampground.author.id.equals(req.user.id)) {
+                    next();
+                } else {
+                    res.redirect("back")
+                }
+            }
+        })
+    } else {
+        res.redirect("back")
+    }
 }
 
 module.exports = router
